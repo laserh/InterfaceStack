14a0e69bbf90124a70e154131e473449ba07e2b2
hlaser <laser.h21@htlwienwest.at>
2023-04-24 18:00:44 +0200

    Stack interface implemented

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,28 @@
-public class Main {
+import javax.naming.OperationNotSupportedException;
 
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("Audi", "45000", 2023 );
+        Car car2 = new Car("BMW", "65000", 2023);
+//        NameStack stack = new NameStack();
+//        stack.push("Max Mustermann");
+//        stack.push("Anna Alfredo");
+//        stack.push("Mustafa Malakay");
+//        System.out.println(stack.getCount());
+//        try {
+//            System.out.println(stack.pop());
+//        } catch (OperationNotSupportedException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(stack.getCount());
+//        System.out.println(stack.peek());
+//        stack.clear();
+//        System.out.println(stack.getCount());
+//        try {
+//            System.out.println(stack.pop());
+//        }
+//        catch (OperationNotSupportedException e) {
+//            System.out.println("Fehler!");
+//        }
+    }
 }
